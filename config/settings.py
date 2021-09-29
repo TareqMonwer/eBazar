92d758209b7f35f951b73697fc3c67e17a798d05
TareqMonwer <tareqmonwer137@gmail.com>
2021-09-29 22:36:54 +0600

    :sparkles: create cart context processor.

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # custom context processors
+                'cart.context_processors.cart',
             ],
         },
     },
